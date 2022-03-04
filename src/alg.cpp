69a403cd8b1884d1247b784b2acf926b832e7171
Victoria Terganova <94222399+Terganova21@users.noreply.github.com>
2022-03-04 18:35:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -17,8 +17,8 @@ double pown(double value, uint16_t n) {
 }
 
 uint64_t fact(uint16_t n) {
-  uint16_t result = 1;
-  for (uint16_t i = 1; i <= n; i++)
+  uint64_t result = 1;
+  for (uint64_t i = 1; i <= n; i++)
     result *= i;
   return result;
 }
